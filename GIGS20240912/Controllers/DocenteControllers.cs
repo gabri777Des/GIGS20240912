4e4306f95f74d9045ef351d5e078590d94059fd3
gabri777Des <gabrisac2006@gmail.com>
2024-09-12 14:28:12 -0600

    Finalizado

---
+++ GIGS20240912/Controllers/DocenteControllers.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GIGS20240912.Models;
+
+namespace GIGS20240912.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocenteControllers : ControllerBase
+    {
+       // Declaración de una lista estática de objetos
+       // "Docente" para almacenar docentes.
+       static List<Docente> docentes = new List<Docente>();
+
+        // Definición de un método HTTP GET
+        // que vuelve una colección de docentes
+        [HttpGet]
+        public IEnumerable<Docente> Get()
+        {
+          return docentes;
+        }
+
+        // Definición de un método HTTP GET
+        // que recibe un ID como parámetro y devuelve un docente específico.
+        [HttpGet("{id}")]
+        public Docente Get(int id)
+        {
+            var docente = docentes.FirstOrDefault(d => d.Id == id);
+            return docente;
+        }
+
+        // Definición de un método HTTP POST
+        // para agregar un nuevo docente a la lista.
+        [HttpPost]
+        public IActionResult Post([FromBody] Docente docente)
+        {
+            docentes.Add(docente);
+            return Ok();
+        }
+    }
+}
---
+++ GIGS20240912/Models/Docente.cs
@@ -0,0 +1,9 @@
+﻿namespace GIGS20240912.Models
+{
+    public class Docente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
